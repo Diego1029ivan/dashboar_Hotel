91842eb5af8e808ea18d624bbbb6d99b88ade50e
Diego1029ivan <dialvaradov@alumno.unsm.edu.pe>
2022-11-30 11:34:33 -0500

    2

--- crud/crudReserva/editar_reserva.php
+++ crud/crudReserva/editar_reserva.php
@@ -1,57 +1,5 @@
 <?php
-if($_SERVER['REQUEST_METHOD']=="POST") { // Modifica los datos de acuerdo a las cajas de texto del formulario
-    
-    $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'http://localhost:75/api/usuarios/'.$_GET['id'],
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'PUT',
-            CURLOPT_POSTFIELDS =>'{ 
-                
-                "nombre":"'.$_POST['nombre'].'",
-                "username":"'.$_POST['username'].'",
-                "apellido":"'.$_POST['apellido'].'",
-                "email":"'.$_POST['email'].'",
-                "celular":"'.$_POST['celular'].'"
-                
-            }',
-            CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json'
-            ),
-        ));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        $data = json_decode($response, true);
-       header('Location: ../usuarios_hotel.php');
-    }else{
 
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-        CURLOPT_URL => 'http://localhost:75/api/usuarios/'.$_GET['id'],
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-
-        $response = curl_exec($curl);
-
-        curl_close($curl);
-        $data = json_decode($response); 
-        //var_dump($data);
-
-            //echo $data->username;
-
-    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
